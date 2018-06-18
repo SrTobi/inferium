0086728dbaff21f72702fc274aa2162e46f3fd21
SrTobi <code.databyte@gmail.com>
2018-06-18 12:18:35 +0200

    removed node-prolude source content because it's too long

--- extras/node-prelude/src/main/scala/inferium/NodePrelude.scala
+++ extras/node-prelude/src/main/scala/inferium/NodePrelude.scala
@@ -7,14 +7,12 @@ object NodePrelude {
     def source: String = NodePreludeSource.content
     def json: PreludeData = {
         val data = upickle.json.read(source)
+        val obj = data.obj
 
-        data match {
-            case Js.Obj(obj) =>
-                List("global", "modules", "types") flatMap { obj get _ } match {
-                    case (global@Js.Obj(_)) :: (modules@Js.Arr(_)) :: (types@Js.Arr(_)) :: List.empty =>
-                        PreludeData(global, modules, types)
-                }
-        }
-
+        PreludeData(
+            obj("global").obj,
+            obj("modules").arr,
+            obj("types").arr
+        )
     }
 }
